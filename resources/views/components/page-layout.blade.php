3324f52e6210a16cf8155b149b2349cc611cbb54
Shuga8 <lotocharles8@gmail.com>
2024-10-03 21:10:49 +0100

    frontend design init...

---
+++ resources/views/components/page-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+        <title>VicronPlus</title>
+        @vite('resources/js/app.js')
+    </head>
+
+    <body>
+        {{ $slot }}
+    </body>
+
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="">
+<x-page-layout>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>VicronPlus</title>
-    </head>
-
-    <body>
-
-    </body>
-
-</html>
+</x-page-layout>
