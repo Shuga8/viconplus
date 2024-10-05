2a39687228aea860bad355b1dd9a726b5b77b87f
Shuga8 <lotocharles8@gmail.com>
2024-10-05 14:54:33 +0100

    about vicron investment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
                 personalized investment plan, tailored to your unique financial goals and objectives.
             </p>
 
-            <div class="mt-10 mb-3 px-14">
+            <div class="mt-10 mb-3 px-14 flex justify-center">
                 <button
                     class="flex flex-row place-items-center gap-x-3 outline-none border-0  bg-blue-800 hover:bg-blue-700 text-white px-7 py-3 rounded-lg text-left mx-auto md:mx-0">
                     <span class="uppercase">Register</span>
@@ -69,7 +69,7 @@
             <div class="process pt-2 pb-5 px-4">
                 <div
                     class="process-icon mx-auto w-14 h-14 flex place-items-center justify-center mt-2 rounded-full bg-white bg-opacity-10 mb-6">
-                    <x-ri-exchange-funds-line class="text-blue-600 w-10 h-10" />
+                    <x-mdi-finance class="text-blue-600 w-10 h-10" />
                 </div>
 
                 <h4 class="text-white uppercase text-center font-bold text-lg">Start Investing</h4>
@@ -82,6 +82,47 @@
 
         </div>
 
+    </section>
+
+    <section class="about-investment py-10">
+        <h3 class="text-center font-bold text-xl uppercase">About Vicron Investment</h3>
+
+        <div class="block md:flex md:gap-x-7 md:gap-y-3 md:justify-between py-6">
+            <div class="bg-white bg-opacity-10 rounded-lg flex-grow basis-60 py-4 px-5 leading-8 block">
+                Investing on VicronPlus offers a world of benefits, empowering you to take control of your financial
+                future. By joining our platform, you'll gain access to a wide range of investment opportunities, expert
+                guidance, and cutting-edge tools to help you achieve your financial goals.
+                <p class="text-xl font-bold mt-4 mb-2">Key Features.</p>
+
+                <ul class="px-2 py-1 ">
+                    <li class="mb-2 text-sm leading-9">
+                        <b>Diversified Investment Options:</b> Explore a variety of assets, including stocks, bonds,
+                        ETFs, and more, to create a balanced portfolio that suits your risk tolerance and investment
+                        objectives.
+                    </li>
+
+                    <li class="mb-2 text-sm leading-9">
+                        <b>Convenience and Accessibility:</b> Invest on-the-go with our user-friendly platform,
+                        available 24/7, and manage your portfolio with ease.
+                    </li>
+
+                    <li class="mb-2 text-sm leading-9">
+                        <b>Security and Transparency:</b> Enjoy peace of mind with our robust security measures and
+                        transparent fee structures.
+                    </li>
+                </ul>
+
+                <div class="my-6 px-3">
+                    <button
+                        class="px-4 py-2 bg-blue-700 text-white rounded-3xl flex flex-row gap-x-3 place-items-center hover:bg-blue-400"><span>Read
+                            more</span> <x-mdi-skip-next-circle-outline class="w-5 h-5 text-white" /></button>
+                </div>
+            </div>
+
+            <div class="hidden md:flex front flex-grow basis-60 justify-center place-items-center">
+                <img src="{{ asset('front.png') }}" alt="" class="w-80 h-80">
+            </div>
+        </div>
     </section>
 
 </x-page-layout>
